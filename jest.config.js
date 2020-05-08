da7be0bf17a26a42ec329c394422b0a3633bcbe6
mefechoel <michel.strelow97@gmail.com>
2020-05-08 15:19:35 +0200

    Collect test coverage

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  transform: {
+    "^.+\\.js$": "babel-jest",
+    "^.+\\.svelte$": "svelte-jester",
+  },
+  testRegex: "/test/.*\\.(test)\\.m?jsx?$",
+  moduleFileExtensions: ["js", "mjs", "svelte"],
+  setupFilesAfterEnv: ["@testing-library/jest-dom/extend-expect"],
+  moduleDirectories: ["node_modules", "src"],
+  collectCoverageFrom: ["src/**/*"],
+  collectCoverage: true,
+  coverageDirectory: "coverage-jest",
+};
