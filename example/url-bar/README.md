f32db3c333838340da12f796fe3b6835ba035da8
mefechoel <michel.strelow97@gmail.com>
2020-05-10 16:52:27 +0200

    Add UrlBar example

---
+++ example/url-bar/README.md
@@ -0,0 +1,35 @@
+## Custom URL Bar
+
+An example mimicing the browsers URL bar.
+
+The example uses:
+
+#### `<Router />`
+
+With a nested Router (`Blog.svelte`).
+
+#### `<Route />`
+
+Using params, wildcards and a default Route.
+
+#### `<Links />`
+
+Using relative links.
+
+### `useNavigate()`
+
+For programmatic navigation back and forward through the history stack.
+
+### `useLocation()`
+
+For reacting to changes of the location.
+
+### Try it out
+
+```bash
+git clone https://github.com/mefechoel/svelte-navigator.git
+cd svelte-navigator/example/url-bar
+yarn install # if you're using npm run 'npm install'
+yarn start # 'npm start'
+# open browser at 'http://localhost:6064'
+```
---
+++ example/url-bar/src/UrlBar.svelte
@@ -0,0 +1,36 @@
+<script>
+  import { useLocation, useNavigate } from "svelte-navigator";
+
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  let url;
+
+  $: {
+    url = $location.pathname;
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    navigate(url);
+  }
+
+  function handleInput(e) {
+    url = e.target.value;
+  }
+</script>
+
+<style>
+  form {
+    display: inline-block;
+  }
+</style>
+
+<div>
+  <button type="submit" on:click={() => navigate(-1)}>&lt;-</button>
+  <button type="submit" on:click={() => navigate(1)}>-&gt;</button>
+  <form on:submit={handleSubmit}>
+    <input type="text" value={url} on:input={handleInput} />
+    <button type="submit">GO</button>
+  </form>
+</div>
