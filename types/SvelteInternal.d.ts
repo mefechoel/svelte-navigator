34ffa285123f40fb4b4c7cc7a96d538d9990ca36
mefechoel <michel.strelow97@gmail.com>
2022-06-04 14:46:47 +0200

    Fix TS component definitions

--- types/SvelteInternal.d.ts
+++ types/SvelteInternal.d.ts
@@ -1,7 +1,12 @@
 import type { SvelteComponentTyped } from "svelte";
 
-// eslint-disable-next-line @typescript-eslint/ban-types
-export declare class LocalComponent<Props = {}, Slots = {}> extends SvelteComponentTyped<Props> {
+export declare class LocalComponent<
+	// eslint-disable-next-line @typescript-eslint/ban-types
+	Props = {},
+	// eslint-disable-next-line @typescript-eslint/ban-types
+	Slots = {}
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+> extends SvelteComponentTyped<Props, any, Slots> {
 	// eslint-disable-next-line camelcase
 	$$prop_def: Props;
 
